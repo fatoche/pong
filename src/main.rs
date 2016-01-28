3ca17ad3c19e845b5f87e367665973c494da7167
Clara Scherer <git20357@s.cherer.de>
2016-01-28 19:20:10 +0100

    Ball der an Displaygrenzen reflektiert wird

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,9 @@ fn main() {
 
     const DISPLAY_WIDTH: i32 = 640;
     const DISPLAY_HEIGHT: i32 = 480;
-    const IMAGE_HEIGHT: i32 = 300;
-    const IMAGE_WIDTH: i32 = 200;
+    const IMAGE_HEIGHT: i32 = 120;
+    const IMAGE_WIDTH: i32 = 50;
+    const BALL_RADIUS: i32 = 20;
 
     let mut display = jamkit::Graphics::init("test", DISPLAY_WIDTH as u32, DISPLAY_HEIGHT as u32);
     let texture = jamkit::Texture::load(&display, "bild.png");
@@ -32,6 +33,12 @@ fn main() {
     let mut direction = Richtung::Stopp;
     let mut y = 0;
     const STEP: i32 = 5;
+
+    // Ballvariables
+    let mut ball_x = DISPLAY_WIDTH/2;
+    let mut ball_y = DISPLAY_HEIGHT/2;
+    let mut velocity_x = -5;
+    let mut velocity_y = -3;
 
     'main: loop {
         for event in display.poll_events() {
@@ -48,8 +55,24 @@ fn main() {
             _ => (),
         }
 
+        ball_x += velocity_x;
+        ball_y += velocity_y;
+        // reflect ball from walls
+        const RIGHT_BORDER: i32 = DISPLAY_WIDTH - BALL_RADIUS;
+        const LOWER_BORDER: i32 = DISPLAY_HEIGHT - BALL_RADIUS;
+        match ball_x {
+            BALL_RADIUS...RIGHT_BORDER => (),
+            _ => velocity_x *= -1,
+        }
+        match ball_y {
+            BALL_RADIUS...LOWER_BORDER => (),
+            _ => velocity_y *= -1,
+        }
+
         let mut frame = jamkit::Frame::start(&display);
         frame.draw(&texture, None, [0, y, IMAGE_WIDTH, y + IMAGE_HEIGHT]);
+        frame.draw(&texture, None, [ball_x - BALL_RADIUS, ball_y - BALL_RADIUS,
+                                    ball_x + BALL_RADIUS, ball_y + BALL_RADIUS]);
         frame.finish();
     }
 }
